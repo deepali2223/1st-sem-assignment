221869baf9427042c3597ede0da4a1d6b8a93283
deepali2223 <120616967+deepali2223@users.noreply.github.com>
2023-01-12 19:46:18 +0530

    Update frequency of nos.cpp

--- frequency of nos.cpp
+++ frequency of nos.cpp
@@ -1,16 +1,27 @@
-#include<stdio.h>
+#include <stdio.h>
+#define BASE 10
 int main()
 {
-int n1=0,n2=1,n3,i,number;
-printf("Enter the number of elements:");
-scanf("%d",&number);
-printf("\n%d %d",n1,n2);
-for(i=2;i<number;++i)
+long long num, n;
+int i, lastDigit;
+int freq[BASE];
+printf("Enter any number: ");
+scanf("%lld", &num);
+for(i=0; i<BASE; i++)
 {
-n3=n1+n2;
-printf(" %d",n3);
-n1=n2;
-n2=n3;
+freq[i] = 0;
+}
+n = num;
+while(n != 0)
+{
+lastDigit = n %10;
+n /= 10;
+freq[lastDigit]++;
+}
+printf("Frequency of each digit in %lld is: \n", num);
+for(i=0; i<BASE; i++)
+{
+printf("Frequency of %d = %d\n", i, freq[i]);
 }
 return 0;
 }
