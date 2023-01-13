478dfe74d6f1638587266d4cc938afcc49acca70
deepali2223 <120616967+deepali2223@users.noreply.github.com>
2023-01-13 21:59:08 +0530

    Update prime no.cpp

--- prime no.cpp
+++ prime no.cpp
@@ -1,16 +1,23 @@
-#include<stdio.h>
-int main()
-{
-  int st,stop,sum=0;
-  printf("enter the starting no");
-  scanf("%d",&st);
-  printf("enter the end point");
-  scanf("%d",&stop);
-  while(st<stop)
-  {if (st%2==0)
-  sum= sum + st;
-  st ++;
+#include <stdio.h>
+
+int main() {
+
+  int n, i, flag = 0;
+  printf("Enter a positive integer: ");
+  scanf("%d", &n);
+  if (n == 0 || n == 1)
+    flag = 1;
+
+  for (i = 2; i <= n / 2; ++i) {
+    if (n % i == 0) {
+      flag = 1;
+      break;
+    }
   }
-   printf("the sum of all even no is%d",sum); 
-	return 0;
+  if (flag == 0)
+    printf("%d is a prime number.", n);
+  else
+    printf("%d is not a prime number.", n);
+
+  return 0;
 }
