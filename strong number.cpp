5670bebe11715b8814b6fa85d229a0d89475ba3c
deepali2223 <120616967+deepali2223@users.noreply.github.com>
2023-01-13 21:56:51 +0530

    Update strong number.cpp

--- strong number.cpp
+++ strong number.cpp
@@ -1,9 +1,19 @@
-#include<stdio.h>
-int main()
-{int a=3,b=5;
-a = a ^ b;
-
-printf("%d\n",a);
-printf("%d",b);
-return 0;
-}
+#include<stdio.h>  
+void main()  
+{   
+int num, rem, sum = 0, i;    
+printf("Enter a number\n");  
+scanf("%d", &num);      
+for(i = 1; i < num; i++)  
+                     {  
+                             rem = num % i;  
+                             if (rem == 0)  
+                                        {  
+                                     sum = sum + i;  
+                                         }  
+                        }  
+if (sum == num)  
+                      printf(" %d is a Perfect Number");  
+           else  
+                      printf("\n %d is not a Perfect Number");  
+}  
